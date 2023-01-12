7478e8964b4955be0517a5c41ad4bade0a850cb9
Alan Yang <ayang1097@gmail.com>
2023-01-12 14:34:12 -0800

    Update sherlock_demo.sh

--- sherlock_demo/sherlock_demo.sh
+++ sherlock_demo/sherlock_demo.sh
@@ -29,4 +29,5 @@ export GUROBI_HOME="/share/software/user/restricted/gurobi/9.0.3_py36"
 lscpu
 
 # Run script
-julia sherlock_demo.jl 0 127 66 20 SOS 10 100000 100000 10 false
+julia sherlock_demo.jl 0 127 66 20 SOS 10 100000 100000 10 8 false
+# julia sherlock_demo.jl 0 1023 31 20 SOS 10 100000 100000 10 8 false
